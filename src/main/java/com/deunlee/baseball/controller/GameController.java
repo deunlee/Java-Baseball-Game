7a9c7831e8e31f3d781afb305d82e4a342576020
Deun Lee <deunlee@deunlee.com>
2026-01-28 17:28:21 +0900

    feat: 컨트롤러 시작 로직 구현

--- src/main/java/com/deunlee/baseball/controller/GameController.java
+++ src/main/java/com/deunlee/baseball/controller/GameController.java
@@ -1,8 +1,43 @@
 package com.deunlee.baseball.controller;
 
+import com.deunlee.baseball.model.BaseballGame;
+import com.deunlee.baseball.model.BaseballNumberGenerator;
+import com.deunlee.baseball.view.TextInputView;
+import com.deunlee.baseball.view.TextOutputView;
+
 public class GameController {
+    private final TextInputView  inputView;
+    private final TextOutputView outputView;
+
+    public GameController() {
+        this.inputView  = new TextInputView();
+        this.outputView = new TextOutputView();
+    }
 
     public void run() {
-        System.out.println("test");
+        outputView.printStartMessage();
+        boolean isContinue = true;
+        while (isContinue) {
+            playGame();
+            isContinue = shouldRestart();
+        }
+    }
+
+    private void playGame() {
+        BaseballGame game = new BaseballGame(new BaseballNumberGenerator());
+    }
+
+    private boolean shouldRestart() {
+        outputView.printRestartPrompt();
+        try {
+        } catch (IllegalArgumentException e) {
+            handleError(e);
+            return shouldRestart();
+        }
+    }
+
+
+    private void handleError(IllegalArgumentException e) {
+        outputView.printError(e.getMessage());
     }
 }
