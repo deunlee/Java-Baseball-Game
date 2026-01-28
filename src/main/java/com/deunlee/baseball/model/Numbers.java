9f02b63e78e5b4566aaa496ff7f22f92220b4027
Deun Lee <deunlee@deunlee.com>
2026-01-28 17:56:50 +0900

    feat: 숫자 모델 추가

---
+++ src/main/java/com/deunlee/baseball/model/Numbers.java
@@ -0,0 +1,15 @@
+package com.deunlee.baseball.model;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Numbers {
+
+    private final List<Integer> digits;
+
+    public Numbers(List<Integer> digits) {
+        this.digits = new ArrayList<>(digits);
+    }
+
+}
