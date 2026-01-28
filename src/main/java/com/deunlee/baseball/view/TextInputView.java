fc3614400e47ef4519dcf04560197881ca99c5f5
Deun Lee <deunlee@deunlee.com>
2026-01-28 18:29:52 +0900

    feat: 입력 뷰에 검증 로직 추가

--- src/main/java/com/deunlee/baseball/view/TextInputView.java
+++ src/main/java/com/deunlee/baseball/view/TextInputView.java
@@ -1,4 +1,26 @@
 package com.deunlee.baseball.view;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class TextInputView {
+
+    private void validateLength(final String input, int length) {
+        if (input.length() != length) {
+            throw new IllegalArgumentException(length + "자리 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private void validateDigit(final char c) {
+        if (!Character.isDigit(c)) {
+            throw new IllegalArgumentException("숫자만 입력해야 합니다.");
+        }
+    }
+
+    private void validateRestartDecision(int decision) {
+        if (decision != 1 && decision != 2) {
+            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+        }
+    }
 }
