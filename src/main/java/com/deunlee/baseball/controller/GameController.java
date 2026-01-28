c41f528b3e72684da93871a96e48c574024fee63
Deun Lee <deunlee@deunlee.com>
2026-01-28 18:12:19 +0900

    feat: 컨트롤러에서 사용자 입력 처리

--- src/main/java/com/deunlee/baseball/controller/GameController.java
+++ src/main/java/com/deunlee/baseball/controller/GameController.java
@@ -3,6 +3,7 @@ package com.deunlee.baseball.controller;
 import com.deunlee.baseball.model.BaseballGame;
 import com.deunlee.baseball.model.BaseballNumberGenerator;
 import com.deunlee.baseball.model.ComparisonResult;
+import com.deunlee.baseball.model.Numbers;
 import com.deunlee.baseball.view.TextInputView;
 import com.deunlee.baseball.view.TextOutputView;
 
@@ -36,15 +37,20 @@ public class GameController {
     private boolean shouldRestart() {
         outputView.printRestartPrompt();
         try {
+            return inputView.readRestartDecision();
         } catch (IllegalArgumentException e) {
             handleError(e);
-            return shouldRestart();
+            return shouldRestart(); // 잘못된 값을 입력하면 다시 물어봄
         }
     }
 
     private ComparisonResult processGuess(BaseballGame game) {
         try {
             outputView.printGuessPrompt();
+            final Numbers playerNumbers = new Numbers(inputView.readPlayerGuess());
+            final ComparisonResult result = game.checkResult(playerNumbers);
+            outputView.printResult(result);
+            return result;
         } catch (IllegalArgumentException e) {
             handleError(e);
             return null;
