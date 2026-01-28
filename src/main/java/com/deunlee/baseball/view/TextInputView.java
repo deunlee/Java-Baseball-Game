0a0d16b4218c253136397cae7319a8773af17ef7
Deun Lee <deunlee@deunlee.com>
2026-01-28 18:31:10 +0900

    feat: 입력 뷰 구현

--- src/main/java/com/deunlee/baseball/view/TextInputView.java
+++ src/main/java/com/deunlee/baseball/view/TextInputView.java
@@ -5,6 +5,40 @@ import java.util.List;
 import java.util.Scanner;
 
 public class TextInputView {
+    private static final int REQUIRED_LENGTH  = 3;
+    private static final int RESTART_DECISION = 1;
+
+    public List<Integer> readPlayerGuess() {
+        final String input = readLine();
+        validateLength(input, REQUIRED_LENGTH);
+        return convertToDigits(input);
+    }
+
+    public boolean readRestartDecision() {
+        final String input = readLine();
+        validateLength(input, 1);
+        int decision = convertToDigit(input.charAt(0));
+        validateRestartDecision(decision);
+        return decision == RESTART_DECISION;
+    }
+
+    private String readLine() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    private int convertToDigit(char c) {
+        validateDigit(c);
+        return c - '0';
+    }
+
+    private List<Integer> convertToDigits(final String input) {
+        List<Integer> digits = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            digits.add(convertToDigit(c));
+        }
+        return digits;
+    }
 
     private void validateLength(final String input, int length) {
         if (input.length() != length) {
